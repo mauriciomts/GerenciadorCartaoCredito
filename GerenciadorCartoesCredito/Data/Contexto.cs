a2d834a670bdb1c73d8bcb0c36a0e51372963e43
Mauricio Matos <mauriciomts@live.com>
2020-09-21 12:00:38 -0400

    Migrations

---
+++ GerenciadorCartoesCredito/Data/Contexto.cs
@@ -0,0 +1,20 @@
+﻿using GerenciadorCartoesCredito.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GerenciadorCartoesCredito.Data
+{
+    public class Contexto : DbContext
+    {
+        public DbSet<Cartao> Cartoes { get; set; }
+        public DbSet<Gasto> Gastos { get; set; }
+
+        public Contexto(DbContextOptions<Contexto> opcoes) : base(opcoes)
+        {
+
+        }
+    }
+}
